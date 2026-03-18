3422a04ba342bc63dcd39f43f27be04c41d1116e
Aidan <amleary888@gmail.com>
2026-03-18 16:56:23 +0000

    sendtopc

--- stocksClass.py
+++ stocksClass.py
@@ -3,6 +3,8 @@ import sklearn
 import yfinance
 import numpy as np
 
+complete_stocks = pd.read_csv("out.csv").set_index("Date")
+
 class indexPredictor:
 
     # Struct which defines an index predicting model, defined by explanatory stocks and their betas.
@@ -13,15 +15,31 @@ class indexPredictor:
             self.explanatoryBetas = explanatoryBetas
             self.interval = interval
 
+        def mToString(self):
+            print(type(self.index))
+            print(type(self.explanatoryStocks))
+            print(type(self.explanatoryBetas))
+            print(type(self.interval))
+            print("Index: " + self.index + "\nExplanatory stocks: " + " ".join(self.explanatoryStocks.to_list()) +
+                   "\nExplanatory betas: " + np.array2string(self.explanatoryBetas))
+
     #Note: Stocks will include indexes such as S&P. stockIndexData is entirely static, only ever read
-    def __init__(self, stocks, indexes, start_ = "2016-1-1", end_ = "2026-1-1", interval_ = "1d", type = 'High'):
-        self._stockData = yfinance.download(stocks, start = start_, end = end_, interval = interval_).get(type)
-        self._indexData = yfinance.download(indexes, start = start_, end = end_, interval = interval_).get(type)
+    def __init__(self, stocks, indexes, start_ = "2016-1-1", end_ = "2026-1-1", interval_ = "1d", type = 'High', tester = False):
+        if (not tester):
+            self._stockData = yfinance.download(stocks, start = start_, end = end_, interval = interval_).get(type)
+            self._indexData = yfinance.download(indexes, start = start_, end = end_, interval = interval_).get(type)
+        else:
+            self._stockData = complete_stocks
+            self._indexData = yfinance.download("^GSPC", start = "2016-1-1", end = "2026-1-1", interval = "1d").get('High')
+
 
     #Given a set of stocks, a target index, and a number of explanatory stocks to return, returns an indexPredictorModel.
-    def buildIndexPredictorM(self, index, stocks, number, interval = None):
+    def buildIndexPredictor(self, index, stocks, number, interval = None, allStocks = False):
 
-        stock_raw_data = self._stockData.get(stocks)
+        if (allStocks):
+            stock_raw_data = self._stockData
+        else:
+            stock_raw_data = self._stockData.get(stocks)
         stock_diff_logs = (stock_raw_data.shift(-1).apply(np.log) - stock_raw_data.shift(1).apply(np.log))[1:-1]
         
         index_raw_data = self._indexData.get(index)
@@ -60,20 +78,36 @@ class indexPredictor:
             explanatory_betas = best_beta
             stock_diff_logs.drop(best_stock, axis = 1)
 
-        return self.indexPredictorModel(index, best_explanatory, explanatory_betas, interval)
+        return self.indexPredictorModel(index, best_explanatory.columns, explanatory_betas, interval)
     
     def modelQualityOfFit(self, model, interval = None):
 
         new_model = sklearn.linear_model.LinearRegression()
-        new_model.intercept = 0
+        new_model.intercept_ = 0
         new_model.coef_ = model.explanatoryBetas
+        
+        index_raw_data = self._indexData.get(model.index)
+        index_diff_logs = (index_raw_data.shift(-1).apply(np.log) - index_raw_data.shift(1).apply(np.log))[1:-1]
 
-        if (interval != None):
-            index_interval = self._indexData.loc(interval)
-            stock_interval = self._stockData.loc(interval)
+        # if (interval != None):
+        #     index_interval = self._indexData.loc(interval)
+        #     stock_interval = self._stockData.loc(interval)
 
-            return sklearn.metrics.r2_score(index_interval.get(model.index), new_model.predict(stock_interval.get(model.explanatoryStocks)))
+        #     return sklearn.metrics.r2_score(index_diff_logs, new_model.predict(stock_interval.get(model.explanatoryStocks)))
     
-        return sklearn.metrics.r2_score(self._indexData.get(model.index), new_model.predict(self._stockData.get(model.explanatoryStocks)))
+        return sklearn.metrics.r2_score(index_diff_logs, new_model.predict(self._stockData.get(model.explanatoryStocks)))
     
+    def purchaseStocks(self, model, date, moneySpent):
 
+        stock_raw_data = self._stockData.get(model.explanatoryStocks)
+
+        stock_value = stock_raw_data.get(date)
+
+        ratios = np.linalg.norm (model.explanatoryBetas) * moneySpent
+
+        return stock_value * ratios
+
+temp = indexPredictor(0,0, tester=True)
+bruh = temp.buildIndexPredictor("^GSPC",0,3,allStocks=True)
+print(bruh.mToString())
+print(temp.modelQualityOfFit(bruh))
